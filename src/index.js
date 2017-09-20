64d8ed97e47bd8ff984665cb0902522e728f5338
Nicolas Vanhoren <n.vanhoren@evs.com>
2017-09-20 15:51:01 +0200

    Added react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import _ from 'lodash';
 import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function component() {
-    var element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.querySelector('#root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const packageJson = require('./package.json');
 
 module.exports = {
@@ -9,29 +10,42 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Webpack Test'
+            template: 'src/index.ejs'
         })
     ],
     module: {
         rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }, {
             test: /\.scss$/,
             use: [{
                 loader: "style-loader" // creates style nodes from JS strings
             }, {
                 loader: "css-loader", // translates CSS into CommonJS
                 options: {
-                  alias: (() => {
-                      const m = {};
-                      m[packageJson.name] = path.resolve('.');
-                      return m;
-                  })(),
+                    alias: (() => {
+                        const m = {};
+                        m[packageJson.name] = path.resolve(
+                            '.');
+                        return m;
+                    })(),
                 },
             }, {
                 loader: "sass-loader" // compiles Sass to CSS
             }]
         }, {
-            test: [/\.woff2$/, /\.eot$/, /\.svg$/, /\.ttf$/, /\.woff$/],
+            test: [/\.woff2$/, /\.eot$/, /\.svg$/, /\.ttf$/,
+                /\.woff$/
+            ],
             loader: 'file-loader',
         }, ]
     }
