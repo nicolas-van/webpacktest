43107f085aeb2508f77b3d71583449920d86acc1
Nicolas Vanhoren <n.vanhoren@evs.com>
2017-09-20 16:00:29 +0200

    Added axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
 import './index.scss';
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.querySelector('#root')
-);
+axios.get('/testapi').then((test) => {
+    ReactDOM.render(
+      <div>
+        <h1>Hello, world!</h1>
+        <p>{test.data}</p>
+      </div>,
+      document.querySelector('#root')
+    );
+})
