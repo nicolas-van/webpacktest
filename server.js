5447beae7b08b4c90a7b56f1f76021a7ed36298f
Nicolas Vanhoren <n.vanhoren@evs.com>
2017-09-20 15:11:38 +0200

    Replace koa-file-server by koa-static

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const Koa = require('koa');
 const Router = require('koa-trie-router')
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
 
-app.use(require('koa-file-server')({
-    root: "./dist",
-    index: true,
-}));
+app.use(serve("./dist"));
 
 router.get('/testapi', async function (ctx, next) {
     ctx.body = "test";
